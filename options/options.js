a59e34482ebc14439e8032615946817aa286b190
Crowdbotics <team@crowdbotics.com>
2021-10-18 21:41:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "shiny_mouse_31525",
+  url: "https://shiny_mouse_31525.botics.co",
+  api: "https://shiny_mouse_31525.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
